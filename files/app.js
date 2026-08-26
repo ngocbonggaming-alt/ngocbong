15e004df3eb4c929966868e5b0af58fe8235f4b0
ngocbonggaming-alt <thespike.receiven@gmail.com>
2026-08-26 12:30:09 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -5,22 +5,22 @@ const apps = [
         name: '<strong><em>Delta X Quốc Tế IOS  ✅</em></strong>',
         url: 'https://ipas.cc/e157z0',
         img: 'https://i.postimg.cc/wMkzwh26/IMG-4719.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.735 Cập Nhật Ngày 25/08/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.735 Cập Nhật Ngày 26/08/2026'
     },{ 
         name: '<strong><em>Delta X Quốc Tế IOS Fix Lag  ✅</em></strong>',
         url: 'https://ipas.cc/k4jroo',
         img: 'https://i.postimg.cc/wMkzwh26/IMG-4719.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.733 Cập Nhật Ngày 25/08/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.733 Cập Nhật Ngày 26/08/2026'
     },{ 
         name: '<strong><em>Delta X VNG IOS ✅</em></strong>',
         url: 'https://ipas.cc/4m5b96',
         img: 'https://i.postimg.cc/j5Pt7VJt/IMG-3155.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.735 Cập Nhật Ngày 25/08/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.735 Cập Nhật Ngày 26/08/2026'
     },{ 
         name: '<strong><em>Delta X VNG IOS Fix Lag ✅</em></strong>',
         url: 'https://ipas.cc/fg7og4',
         img: 'https://i.postimg.cc/pXQNsVQs/IMG-3417.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.735 Cập Nhật Ngày 25/08/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.735 Cập Nhật Ngày 26/08/2026'
     },{
         name: '<strong><em> MENU FF IOS ✅</em></strong>',
         url: 'https://ipas.cc/2bwnw7',
